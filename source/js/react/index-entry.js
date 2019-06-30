2d943895b3ef4c9ee8b8c6299d59755f870ca448
sangmi lee <leesangmi@sangmiui-MacBook-Pro.local>
2019-06-30 22:48:37 +0900

    my page

--- source/js/react/index-entry.js
+++ source/js/react/index-entry.js
@@ -5,10 +5,16 @@ import ReactDOM from 'react-dom';
 import TextField from '@material-ui/core/TextField';
 
 import {ImportTestComponent} from './importTest.js';
+import {MyPage} from './mypage.js';
 
 
 const Name = '승승';
 const Greeting = '안녕하세요~ 여기서 테스트용 리액트 컴포넌트를 넣으시오';
+
+const JoinInfo = {
+    'id' : '승승승승',
+    'date' : '2019년 01월 01일',
+}
 
 const handleChange = (e) =>{
     console.log('e.target : ', e.target);
@@ -38,7 +44,17 @@ if($test.length){
 
 const $importTest = $('#importTest');
 if($importTest.length){
+    console.log('importTest')
     ReactDOM.render(
         <ImportTestComponent name={Name}  greeting={Greeting}></ImportTestComponent>
         , $importTest[0]);
+}
+
+const $myInfo = $('.myInfo');
+console.log($myInfo)
+if($myInfo.length){
+    console.log('MyPage')
+    ReactDOM.render(
+        <MyPage name={Name} joinInfo = {JoinInfo}></MyPage>
+        , $myInfo[0]);
 }
